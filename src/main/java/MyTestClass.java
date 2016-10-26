94c076ac8793c38742cfeccbca1534a02547f6bc
Samuel Printz <sam.printz@web.de>
2016-10-26 16:41:40 +0200

    Working Jena SPARQL query example.

--- src/main/java/MyTestClass.java
+++ src/main/java/MyTestClass.java
@@ -1,3 +1,10 @@
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 
@@ -5,14 +12,31 @@ import org.apache.jena.rdf.model.ModelFactory;
 public class MyTestClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!2");
-
-        Model model = ModelFactory.createDefaultModel() ;
-        model.read("data.rdf");
-        model.write(System.out,"TURTLE");
-        
-        
-        System.out.println("Hallo");
+        sparqlTest();
     }
 
+    private static void sparqlTest() {
+        Model model = ModelFactory.createDefaultModel();
+        model.read("data.rdf");
+
+        String queryString =
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+                "SELECT * WHERE { " +
+                " ?person foaf:knows ?person2 ." +
+                " ?person2 foaf:name ?x ." +
+                "}";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+        try {
+            ResultSet results = qexec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                Literal name = soln.getLiteral("x");
+                System.out.println(name);
+            }
+        } finally {
+            qexec.close();
+        }
+    }
 }
